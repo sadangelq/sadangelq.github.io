53a2ebdc6dff3bd1533ae3ffc98c228e331fcf11
sadangelq <114668656+sadangelq@users.noreply.github.com>
2025-02-05 18:09:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Администрация MineCraft проэкта ▪︎ KS ADMIN</title>
+    <link rel="icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<header>
+    <img src="img/favicon.ico" alt="KosmosTimes Logo">
+    <nav>
+        <a href="login.php">Вход🚪</a>
+    </nav>
+</header>
+<main>
+    <div class="welcome">
+        <h2>Идеальный админ сайт для нашего проекта</h2>
+        <p>Воплощайте свои фантазии в реальность, и покоряйте вершины вместе с нами</p>
+        <div class="cta-container">
+            <a href="login.php" class="cta-button">Перейти в панель</a>
+        </div>
+
+    <section class="features">
+    <div class="feature">
+        <h3>Панель Магазина</h3>
+        <p>Удобный интерфейс для управления Minecraft-магазином KosmosTimes. Настраивайте товары, скидки и следите за покупками в реальном времени.</p>
+    </div>
+    <div class="feature">
+        <h3>Документация</h3>
+        <p>Подробная вики с инструкциями серверам и проекта. Найдите ответы на любые вопросы в одном месте.</p>
+    </div>
+    <div class="feature">
+        <h3>Единая Система</h3>
+        <p>Интеграция всех сервисов проекта: от магазина до панели управления. Удобство и простота в каждом шаге.</p>
+    </div>
+</section>
+</main>
+<footer>
+        <div class="footer-content">
+            <div class="footer-info">
+                <div class="footer-logo-text">
+                    <img src="img/favicon.ico" alt="KS Logo" class="footer-logo">
+                    <span class="footer-logo-name">Copyright © KS ADMIN 2024. Все права защищены. </span>
+                </div>
+                <p>Сайты и Сервера KosmosTimes не относятся к Mojang Studios и Microsoft.</p>
+            </div>
+            <div class="footer-docs">
+                <p>Главное</p>
+                <a href="http://kosmostimes.fun">Главная</a>
+                <a href="https://store.kosmostimes.fun/">Магазин</a>
+                <a href="https://t.me/KosmosTimesHelp_bot">Поддержка</a>
+                <a href="dashboard.php">Панель</a>
+            </div>
+            <div class="footer-contacts">
+                <p>Ссылки:</p>
+                <a href="https://vk.com/kosmostimes">ВКонтакте</a>
+                <a href="https://t.me/KosmosTimes">Telegram</a>
+                <a href="https://discord.gg/c6x2uyUuQz">Discord</a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
